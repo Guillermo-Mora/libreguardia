48410e351dbc0ecdd13ef820cbee2c45e500fd29
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-13 12:36:22 +0100

    Correct implementation of the Entities done. After watching an official video of Kotlin JPA and reading some articles on how to implement it. Data classes should not be used. Currently, only the Zone entity is well implemented. But it serves as a base to corect the rest. Video source: https://www.youtube.com/watch?v=a_6V8xwiv04

--- backend/libreguardia/src/main/kotlin/com/libreguardia/api/LibreguardiaApplication.kt
+++ backend/libreguardia/src/main/kotlin/com/libreguardia/api/LibreguardiaApplication.kt
@@ -1,5 +1,7 @@
 package com.libreguardia.api
 
+import com.libreguardia.api.entity.Place
+import com.libreguardia.api.entity.Zone
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
--- backend/libreguardia/src/main/kotlin/com/libreguardia/api/entity/Zone.kt
+++ backend/libreguardia/src/main/kotlin/com/libreguardia/api/entity/Zone.kt
@@ -8,19 +8,29 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
+import org.hibernate.Hibernate
 import java.util.UUID
 
 @Entity
 @Table(name = "tbl_zone")
-data class Zone (
+class Zone {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "zone_id")
-    val zoneId: UUID? = null,
+    var zoneId: UUID? = null
 
     @Column(name = "zone_name", nullable = false, unique = true, length = 50)
-    val zoneName: String = "",
+    var zoneName: String = ""
 
     @OneToMany(mappedBy = "zone", fetch = FetchType.LAZY)
-    val places: Set<Place>
-)
+    var places: MutableSet<Place> = mutableSetOf()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Zone
+        return zoneId != null && zoneId == other.zoneId
+    }
+
+    override fun hashCode() = this::class.hashCode()
+}
