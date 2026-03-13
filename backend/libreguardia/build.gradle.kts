ac11ac88ec5afe35997434d3a235e0b6dd9f89d3
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-13 21:44:46 +0100

    Initial implementation of users loggin with Spring Security and JWT

--- backend/libreguardia/build.gradle.kts
+++ backend/libreguardia/build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
 }
 
 kotlin {
---
+++ backend/libreguardia/src/main/kotlin/com/libreguardia/api/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.libreguardia.api.repository
+
+import com.libreguardia.api.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface UserRepository : JpaRepository<User, UUID> {
+    fun findByEmail(email: String): User?
+}
