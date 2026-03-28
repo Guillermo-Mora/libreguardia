826a626ec62a98a5fcabf8059019ed84ac9adb2f
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-28 19:30:19 +0100

    Updated database schema. With this new approach, user are permanents, and schedules are temporary. Now, any table relies on the academic year table, as its not necessary. Time range table removed as it was unnecessary and added complexity. Also, both historic tables for absences and services have been removed. Now, all the registries will be stored in the main tables, wich makes everything simplier and easier to mantain.
    The problem of the database was that every absence relied on the existance of its referenced schedule in order to store any useful information. But this relationship was wrong, as it made that deleting old schedules, changing the, etc, completely altered the absences and services related to them. Making schedules impossible to modify or delete if they had any absence associated. With this new approach, every absence is independent, and has its referenced tables declared, withouth depending on any user schedule. After a lot of time of thinking, tinkering and continously changing things, I think this is the best approach so far. Is the best way to keep the system simple, easy, and escalable and maintainable in time, withouth suffering issues. As the idea of having historic tables added complexity, while the idea of never deleting any schedule would end up with a system full of unused rows, affecting the performance when doing queries to the schedule table.

--- libreguardia.ktor/src/main/resources/db/migration/V1__create_tables.sql
+++ libreguardia.ktor/src/main/resources/db/migration/V1__create_tables.sql
@@ -3,7 +3,8 @@
 -- ========================
 
 CREATE TABLE app_settings (
-                              name VARCHAR(50) PRIMARY KEY,
+                              id UUID PRIMARY KEY,
+                              name VARCHAR(50) NOT NULL UNIQUE,
                               status VARCHAR(50) NOT NULL
 );
 
@@ -18,12 +19,14 @@ CREATE TABLE schedule_template (
 
 CREATE TABLE place_type (
                             id UUID PRIMARY KEY,
-                            name VARCHAR(50) NOT NULL UNIQUE
+                            name VARCHAR(50) NOT NULL UNIQUE,
+                            is_enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE zone (
                       id UUID PRIMARY KEY,
-                      name VARCHAR(50) NOT NULL UNIQUE
+                      name VARCHAR(50) NOT NULL UNIQUE,
+                      is_enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE academic_year (
@@ -35,12 +38,14 @@ CREATE TABLE academic_year (
 
 CREATE TABLE professional_family (
                                      id UUID PRIMARY KEY,
-                                     name VARCHAR(50) NOT NULL UNIQUE
+                                     name VARCHAR(50) NOT NULL UNIQUE,
+                                     is_enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE building (
                           id UUID PRIMARY KEY,
-                          name VARCHAR(50) NOT NULL UNIQUE
+                          name VARCHAR(50) NOT NULL UNIQUE,
+                          is_enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE user_role (
@@ -51,14 +56,8 @@ CREATE TABLE user_role (
 CREATE TABLE schedule_activity (
                                    id UUID PRIMARY KEY,
                                    name VARCHAR(50) NOT NULL UNIQUE,
-                                   generates_service BOOLEAN NOT NULL
-);
-
-CREATE TABLE time_range (
-                            id UUID PRIMARY KEY,
-                            start_time TIME NOT NULL,
-                            end_time TIME NOT NULL,
-                            CONSTRAINT uq_time_range UNIQUE (start_time, end_time)
+                                   generates_service BOOLEAN NOT NULL,
+                                   is_enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 -- ========================
@@ -69,6 +68,7 @@ CREATE TABLE place (
                        id UUID PRIMARY KEY,
                        name VARCHAR(50) NOT NULL UNIQUE,
                        floor VARCHAR(50) DEFAULT NULL,
+                       is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
                        building_id UUID DEFAULT NULL REFERENCES building(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
                        zone_id UUID NOT NULL REFERENCES zone(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
                        place_type_id UUID NOT NULL REFERENCES place_type(id) ON DELETE RESTRICT ON UPDATE RESTRICT
@@ -80,88 +80,67 @@ CREATE TABLE user_tbl (
                           surname VARCHAR(50) NOT NULL,
                           email VARCHAR(50) NOT NULL UNIQUE,
                           phone_number VARCHAR(20) NOT NULL,
-                          is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
                           password VARCHAR(60) NOT NULL,
+                          is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
+                          is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
                           user_role_id UUID NOT NULL REFERENCES user_role(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 CREATE TABLE course (
                         id UUID PRIMARY KEY,
                         name VARCHAR(50) NOT NULL UNIQUE,
+                        is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
                         professional_family_id UUID NOT NULL REFERENCES professional_family(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 CREATE TABLE group_tbl (
                            id UUID PRIMARY KEY,
                            name VARCHAR(50) NOT NULL UNIQUE,
-                           points_multiplier DECIMAL(3,1) NOT NULL DEFAULT 1.0,
+                           points_multiplier DECIMAL(2,1) NOT NULL DEFAULT 1,
+                           is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
                            course_id UUID NOT NULL REFERENCES course(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 
 CREATE TABLE schedule (
                           id UUID PRIMARY KEY,
                           week_day VARCHAR(9) NOT NULL,
+                          start_time TIME NOT NULL,
+                          end_time TIME NOT NULL,
                           group_id UUID REFERENCES group_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
                           schedule_activity_id UUID NOT NULL REFERENCES schedule_activity(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
                           place_id UUID NOT NULL REFERENCES place(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                          time_range_id UUID NOT NULL REFERENCES time_range(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                          user_id UUID NOT NULL REFERENCES user_tbl(id) ON DELETE CASCADE ON UPDATE RESTRICT,
-                          CONSTRAINT uq_schedule UNIQUE (week_day, time_range_id, user_id)
+                          user_id UUID NOT NULL REFERENCES user_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                          CONSTRAINT uq_schedule UNIQUE (week_day, start_time, end_time, user_id)
 );
 
 CREATE TABLE schedule_template_slot (
                                         id UUID PRIMARY KEY,
                                         week_day VARCHAR(9) NOT NULL,
+                                        start_time TIME NOT NULL,
+                                        end_time TIME NOT NULL,
                                         schedule_template_id UUID NOT NULL REFERENCES schedule_template(id) ON DELETE CASCADE ON UPDATE RESTRICT,
                                         group_id UUID REFERENCES group_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
                                         schedule_activity_id UUID REFERENCES schedule_activity(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
                                         place_id UUID REFERENCES place(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                        time_range_id UUID NOT NULL REFERENCES time_range(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                        CONSTRAINT uq_schedule_template_slot UNIQUE (schedule_template_id, week_day, time_range_id)
+                                        CONSTRAINT uq_schedule_template_slot UNIQUE (week_day, start_time, end_time, schedule_template_id)
 );
 
 CREATE TABLE absence (
                          id UUID PRIMARY KEY,
                          date DATE NOT NULL,
-                         schedule_id UUID NOT NULL REFERENCES schedule(id) ON DELETE CASCADE ON UPDATE RESTRICT,
-                         CONSTRAINT uq_absence UNIQUE (date, schedule_id)
-);
-
-CREATE TABLE absence_history (
-                                 id UUID PRIMARY KEY,
-                                 date DATE NOT NULL,
-                                 user_name VARCHAR(50) NOT NULL,
-                                 user_surname VARCHAR(50) NOT NULL,
-                                 user_email VARCHAR(50) NOT NULL,
-                                 place_name VARCHAR(50) NOT NULL,
-                                 schedule_activity_name VARCHAR(50) NOT NULL,
-                                 group_name VARCHAR(50) DEFAULT NULL,
-                                 time_range_start_time TIME NOT NULL,
-                                 time_range_end_time TIME NOT NULL,
-                                 academic_year_id UUID NOT NULL REFERENCES academic_year(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                 absence_id UUID REFERENCES absence(id) ON DELETE SET NULL ON UPDATE RESTRICT,
-                                 user_id UUID REFERENCES user_tbl(id) ON DELETE SET NULL ON UPDATE RESTRICT
+                         start_time TIME NOT NULL,
+                         end_time TIME NOT NULL,
+                         group_id UUID REFERENCES group_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                         schedule_activity_id UUID NOT NULL REFERENCES schedule_activity(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                         place_id UUID NOT NULL REFERENCES place(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                         user_id UUID NOT NULL REFERENCES user_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                         CONSTRAINT uq_absence UNIQUE (date, start_time, end_time, user_id)
 );
 
 CREATE TABLE service_tbl (
                              id UUID PRIMARY KEY,
-                             points_obtained DECIMAL(8,1) NOT NULL DEFAULT 1,
-                             absence_id UUID NOT NULL UNIQUE REFERENCES absence(id) ON DELETE CASCADE ON UPDATE RESTRICT,
-                             cover_user_id UUID DEFAULT NULL REFERENCES user_tbl(id) ON DELETE CASCADE ON UPDATE RESTRICT,
-                             assigned_user_id UUID REFERENCES user_tbl(id) ON DELETE SET NULL ON UPDATE RESTRICT
-);
-
-CREATE TABLE service_history (
-                                 id UUID PRIMARY KEY,
-                                 points_obtained DECIMAL(8,1) NOT NULL,
-                                 cover_user_name VARCHAR(50) NOT NULL,
-                                 cover_user_surname VARCHAR(50) NOT NULL,
-                                 cover_user_email VARCHAR(50) NOT NULL,
-                                 assigned_user_name VARCHAR(50),
-                                 assigned_user_surname VARCHAR(50),
-                                 assigned_user_email VARCHAR(50),
-                                 absence_history_id UUID NOT NULL REFERENCES absence_history(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-                                 service_id UUID REFERENCES service_tbl(id) ON DELETE SET NULL ON UPDATE RESTRICT,
-                                 cover_user_id UUID REFERENCES user_tbl(id) ON DELETE SET NULL ON UPDATE RESTRICT,
-                                 assigned_user_id UUID REFERENCES user_tbl(id) ON DELETE SET NULL ON UPDATE RESTRICT
+                             points_obtained DECIMAL(8,1) NOT NULL,
+                             absence_id UUID NOT NULL UNIQUE REFERENCES absence(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                             cover_user_id UUID DEFAULT NULL REFERENCES user_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                             assigned_user_id UUID REFERENCES user_tbl(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
