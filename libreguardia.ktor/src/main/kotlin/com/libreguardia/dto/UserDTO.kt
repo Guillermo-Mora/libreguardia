add66f81cb2925a54bf7fd069676e394610e23f9
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-31 20:16:55 +0200

    Added UUID, as it will be used to then edit/delete specific user. Is the way to go, as the email can change. And is safe to use the UUIDs, as they are completely random.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/UserDTO.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/UserDTO.kt
@@ -8,22 +8,19 @@ import java.util.UUID
 
 @Serializable
 data class UserResponseDTO(
-    //@Serializable(with = UUIDSerializer::class)
-    //val id: UUID,
+    @Serializable(with = UUIDSerializer::class)
+    val id: UUID,
     val name: String,
     val surname: String,
     val email: String,
     val phoneNumber: String,
-    val password: String,
     val isEnabled: Boolean,
     val isDeleted: Boolean,
     val userRole: String
 )
 
 @Serializable
-data class UserRequestDTO(
-    //@Serializable(with = UUIDSerializer::class)
-    //val id: UUID,
+data class UserCreateDTO(
     val name: String,
     val surname: String,
     val email: String,
@@ -33,14 +30,26 @@ data class UserRequestDTO(
     val userRole: String
 )
 
+@Serializable
+data class UserEditDTO(
+    val name: String?,
+    val surname: String?,
+    val email: String?,
+    val phoneNumber: String?,
+    val password: String?,
+    val newPassword: String?,
+    val isEnabled: Boolean?,
+    val userRole: String?
+)
+
 fun UserRepository.entityToResponse(
     entity: UserEntity
 ) = UserResponseDTO(
+    id = entity.id.value,
     name = entity.name,
     surname = entity.surname,
     email =entity.email,
     phoneNumber = entity.phoneNumber,
-    password = entity.password,
     isEnabled = entity.isEnabled,
     isDeleted = entity.isDeleted,
     userRole = entity.userRole.name
