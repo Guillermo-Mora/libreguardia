a4cc3d8c8b2524ea1e66c85440307d884ef27347
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-31 17:23:43 +0200

    Renamed to plural.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/config/StatusPages.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/config/StatusPages.kt
@@ -6,7 +6,7 @@ import io.ktor.server.application.install
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respondText
 
-fun Application.configureStatusPage() {
+fun Application.configureStatusPages() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
