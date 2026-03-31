d72bc5f3c89dff5d48455a9d1f670afefd0113bb
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-31 20:17:16 +0200

    Added new method to find by id.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/UserRepository.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/UserRepository.kt
@@ -2,25 +2,34 @@ package com.libreguardia.repository
 
 import com.libreguardia.db.UserEntity
 import com.libreguardia.db.UserRoleEntity
-import com.libreguardia.dto.UserRequestDTO
+import com.libreguardia.db.UserTable
+import com.libreguardia.dto.UserCreateDTO
 import com.libreguardia.dto.UserResponseDTO
 import com.libreguardia.dto.entityToResponse
+import org.jetbrains.exposed.v1.core.eq
+import java.util.UUID
 
 
 class UserRepository {
-    suspend fun all(): List<UserResponseDTO> = UserEntity.all().map(::entityToResponse)
-    suspend fun save(
-        userRequestDTO: UserRequestDTO,
+    fun all(): List<UserResponseDTO> = UserEntity.all().map(::entityToResponse)
+    fun getByUUID(
+        uuid: UUID
+    ): UserResponseDTO? {
+        val user = UserEntity.find { UserTable.id eq uuid }.firstOrNull()
+        return if (user != null) entityToResponse(user) else null
+    }
+    fun save(
+        userCreateDTO: UserCreateDTO,
         userRoleEntity: UserRoleEntity,
         hashedPassword: String
     ) {
         UserEntity.new {
-            this.name = userRequestDTO.name
-            surname = userRequestDTO.surname
-            email = userRequestDTO.email
-            phoneNumber = userRequestDTO.phoneNumber
+            this.name = userCreateDTO.name
+            surname = userCreateDTO.surname
+            email = userCreateDTO.email
+            phoneNumber = userCreateDTO.phoneNumber
             password = hashedPassword
-            isEnabled = userRequestDTO.isEnabled
+            isEnabled = userCreateDTO.isEnabled
             userRole = userRoleEntity
         }
     }
