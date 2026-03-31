14460bb2a069bb26cbf26c5970c0265d52c37fc5
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-03-31 20:16:07 +0200

    Used now as main exception handler for the app

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/config/StatusPages.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/config/StatusPages.kt
@@ -1,15 +1,40 @@
 package com.libreguardia.config
 
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.plugins.statuspages.StatusPages
-import io.ktor.server.response.respondText
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.BadRequestException
+import io.ktor.server.plugins.requestvalidation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
+        exception<RequestValidationException> { call, cause ->
+            call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = cause.reasons.joinToString()
+            )
+        }
+        exception<UserRoleNotFoundException> { call, cause ->
+            call.respond(
+                status = HttpStatusCode.NotFound,
+                message = "Role name ${cause.roleName} not found"
+            )
+        }
+        exception<UserNotFoundException> { call, cause ->
+            call.respond(
+                status = HttpStatusCode.NotFound,
+                message = "User with UUID ${cause.uuid} not found"
+            )
+        }
+        exception<BadRequestException> {call, _ ->
+            call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = "Bad Request"
+            )
+        }
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
 }
