8f49b027c877b926a4ea5284e4d7bc786a07fe53
Carpinjod <carpinjod@alu.edu.gva.es>
2026-04-01 14:55:44 +0200

    Add AcademicYearNotFoundException

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/Exception.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/Exception.kt
@@ -3,3 +3,5 @@ package com.libreguardia.exception
 class UserRoleNotFoundException : RuntimeException()
 class ProfessionalFamilyNotFoundException : RuntimeException()
 class ProfessionalFamilyNameAlreadyExistsException : RuntimeException()
+class AcademicYearNotFoundException : RuntimeException()
+class AcademicYearNotFoundException : RuntimeException()
