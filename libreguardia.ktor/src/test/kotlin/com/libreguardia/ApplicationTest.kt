37bbc1900695ae2c8e840491b67a2bf081cc2b13
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-03 22:12:23 +0200

    Implemented and working test to hard delete a user that has future references and a schedule. This guarantees that if a user without historic references in absences and services is tried to be deleted, it will be fully deleted from the database, and not soft-deleted. Because it doesn't has any important referenced information in order to keep it in the database.

--- libreguardia.ktor/src/test/kotlin/com/libreguardia/ApplicationTest.kt
+++ libreguardia.ktor/src/test/kotlin/com/libreguardia/ApplicationTest.kt
@@ -2,7 +2,6 @@ package com.libreguardia
 
 import com.libreguardia.config.configureDatabase
 import com.libreguardia.config.configureDefaultHeaders
-import com.libreguardia.config.configureFlyway
 import com.libreguardia.config.configureRouting
 import com.libreguardia.config.configureSerialization
 import com.libreguardia.config.configureStatusPages
@@ -23,59 +22,66 @@ import io.ktor.client.plugins.resources.post
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.config.*
 import io.ktor.server.testing.*
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.SizedIterable
 import org.jetbrains.exposed.v1.jdbc.select
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.testcontainers.DockerClientFactory
 import org.testcontainers.postgresql.PostgreSQLContainer
+import java.math.BigDecimal
 import kotlin.test.*
+import kotlin.time.Clock
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.minutes
 
 class ApplicationTest {
     @Test
     fun exposedTablesMatchToDatabase() = testApplication {
-        environment {
-            config = ApplicationConfig("resources/application.yaml")
-        }
+        val dbConnection = setupTestDBAndFlyway()
+
         application {
             configureDatabase(
-                url = "jdbc:postgresql://localhost:5432/libreguardia",
-                user = "postgres",
-                password = "libreguardia"
-            )
-            var missingColStatements = listOf<String>()
-            transaction {
-                missingColStatements =
-                    SchemaUtils.addMissingColumnsStatements(
-                        AbsenceTable,
-                        AcademicYearTable,
-                        BuildingTable,
-                        CourseTable,
-                        GroupTable,
-                        PlaceTable,
-                        PlaceTypeTable,
-                        ProfessionalFamilyTable,
-                        ScheduleActivityTable,
-                        ScheduleTable,
-                        ScheduleTemplateTable,
-                        ScheduleTemplateSlotTable,
-                        ServiceTable,
-                        UserTable,
-                        UserRoleTable,
-                        ZoneTable,
-                        AppSettingsTable
-                    )
-            }
-            missingColStatements.forEach { println(it) }
-            assertEquals(
-                expected = emptyList(),
-                actual = missingColStatements
+                url = dbConnection.url,
+                user = dbConnection.user,
+                password = dbConnection.password
             )
         }
+        var missingColStatements = listOf<String>()
+        transaction {
+            missingColStatements =
+                SchemaUtils.addMissingColumnsStatements(
+                    AbsenceTable,
+                    AcademicYearTable,
+                    BuildingTable,
+                    CourseTable,
+                    GroupTable,
+                    PlaceTable,
+                    PlaceTypeTable,
+                    ProfessionalFamilyTable,
+                    ScheduleActivityTable,
+                    ScheduleTable,
+                    ScheduleTemplateTable,
+                    ScheduleTemplateSlotTable,
+                    ServiceTable,
+                    UserTable,
+                    UserRoleTable,
+                    ZoneTable,
+                    AppSettingsTable
+                )
+        }
+        missingColStatements.forEach { println(it) }
+        assertEquals(
+            expected = emptyList(),
+            actual = missingColStatements
+        )
     }
 
     @Test
@@ -121,7 +127,6 @@ class ApplicationTest {
                         UserRoleTable.name eq "ADMIN"
                     }.limit(1).map { it[UserRoleTable.id].value }.first()
             }
-        println(roleUUID)
 
         val createResponse = client.post(UsersAPI()) {
             contentType(ContentType.Application.Json)
@@ -147,38 +152,211 @@ class ApplicationTest {
     }
 
     @Test
-    fun deleteUserWithoutReferences() = testApplication {
+    fun hardDeleteUserWithFutureReferences() = testApplication {
+        val dbConnection = setupTestDBAndFlyway()
         application {
-            val testDB = PostgreSQLContainer("postgres:18.3").apply {
-                withDatabaseName("testdb")
-                withUsername("test")
-                withPassword("test")
-                start()
-            }
-            val dbUrl = testDB.jdbcUrl
-            val dbUser = testDB.username
-            val dbPassword = testDB.password
             configureDatabase(
-                url = dbUrl,
-                user = dbUser,
-                password = dbPassword
+                url = dbConnection.url,
+                user = dbConnection.user,
+                password = dbConnection.password
             )
-            configureFlyway(
-                url = dbUrl,
-                user = dbUser,
-                password = dbPassword
-            )
+            configureStatusPages()
             configureSerialization()
             configureDefaultHeaders()
-            //configureRouting(UserService(UserRepository()))
+            configureRouting(
+                UserService(
+                    UserRepository(),
+                    AbsenceRepository(),
+                    ServiceRepository(),
+                    ScheduleRepository()
+                )
+            )
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+            install(Resources)
+        }
 
-            //Here temporarly, as right now there's no UserRoleRepository
-            withTransaction {
-                UserRoleEntity.new {
-                    name = "ADMIN"
-                }
+        withTransaction {
+            UserRoleEntity.new {
+                name = "ADMIN"
             }
         }
+        val roleUUID =
+            withTransaction {
+                UserRoleTable.select(UserRoleTable.id)
+                    .where {
+                        UserRoleTable.name eq "ADMIN"
+                    }.limit(1).map { it[UserRoleTable.id].value }.first()
+            }
+
+        val createResponse = client.post(UsersAPI()) {
+            contentType(ContentType.Application.Json)
+            setBody(
+                UserCreateDTO(
+                    name = "Juan",
+                    surname = "Martínez Hernández",
+                    email = "juanmaher@edu.gva.es",
+                    phoneNumber = "000000000",
+                    password = "12345678",
+                    isEnabled = true,
+                    userRoleUUID = roleUUID
+                )
+            )
+        }
+
+        assertEquals(
+            expected = HttpStatusCode.Created,
+            actual = createResponse.status
+        )
+        var usersAPI: List<UserResponseDTO> = client.get(UsersAPI()).body()
+        assertTrue { usersAPI.isNotEmpty() }
+        usersAPI.forEach { println(it) }
+
+        val userUUID =
+            withTransaction {
+                UserTable.select(UserTable.id)
+                    .where {
+                        UserTable.name eq "Juan"
+                    }.limit(1).map { it[UserTable.id].value }.first()
+            }
+
+        val userEntity = withTransaction { UserEntity.findById(userUUID)!! }
+
+        val scheduleActivityEntity = withTransaction {
+            ScheduleActivityEntity.new {
+                name = "test"
+                generatesService = true
+                isEnabled = true
+            }
+        }
+
+        val placeTypeEntity = withTransaction {
+            PlaceTypeEntity.new {
+                name = "test"
+                isEnabled = true
+            }
+        }
+
+        val zoneEntity = withTransaction {
+            ZoneEntity.new {
+                name = "test"
+                isEnabled = true
+            }
+        }
+
+        val placeEntity = withTransaction {
+            PlaceEntity.new {
+                name = "test"
+                floor = null
+                isEnabled = true
+                building = null
+                zone = zoneEntity
+                placeType = placeTypeEntity
+            }
+        }
+
+        //Create 2 absences, and its 2 services. One today ending 30 minutes before current time and the other
+        // one day before current date
+
+        //For the soft-delete test, create same 2 absences and 2 services but 1 day later and same day 30
+        // minutes later
+        val dateTimeNow: LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        val timeZone = TimeZone.currentSystemDefault()
+        val dateTomorrw = dateTimeNow.toInstant(timeZone).plus(1.days).toLocalDateTime(timeZone).date
+        val startTimeToday = dateTimeNow.toInstant(timeZone).plus(30.minutes).toLocalDateTime(timeZone).time
+        val endTimeToday = dateTimeNow.toInstant(timeZone).plus(60.minutes).toLocalDateTime(timeZone).time
+        withTransaction {
+            AbsenceEntity.new {
+                date = dateTomorrw
+                startTime = startTimeToday
+                endTime = endTimeToday
+                group = null
+                scheduleActivity = scheduleActivityEntity
+                place = placeEntity
+                user = userEntity
+            }
+            AbsenceEntity.new {
+                date = dateTimeNow.date
+                startTime = startTimeToday
+                endTime = endTimeToday
+                group = null
+                scheduleActivity = scheduleActivityEntity
+                place = placeEntity
+                user = userEntity
+            }
+            val absence1 = AbsenceEntity.new {
+                date = dateTomorrw
+                startTime = dateTimeNow.time
+                endTime = dateTimeNow.time
+                group = null
+                scheduleActivity = scheduleActivityEntity
+                place = placeEntity
+                user = null
+            }
+            val absence2 = AbsenceEntity.new {
+                date = dateTomorrw
+                startTime = dateTimeNow.time
+                endTime = dateTimeNow.time
+                group = null
+                scheduleActivity = scheduleActivityEntity
+                place = placeEntity
+                user = null
+            }
+            ServiceEntity.new {
+                pointsObtained = BigDecimal.ONE
+                absence = absence1
+                coverUser = null
+                assignedUser = userEntity
+            }
+            ServiceEntity.new {
+                pointsObtained = BigDecimal.ONE
+                absence = absence2
+                coverUser = null
+                assignedUser = userEntity
+            }
+            ScheduleEntity.new {
+                weekDay = WeekDay.WEDNESDAY
+                startTime = dateTimeNow.time
+                endTime = dateTimeNow.time
+                group = null
+                scheduleActivity = scheduleActivityEntity
+                place = placeEntity
+                user = userEntity
+            }
+        }
+        var schedules = withTransaction { ScheduleEntity.all() }
+        assertTrue { withTransaction { schedules.count().toInt() } == 1 }
+
+        var absences: SizedIterable<AbsenceEntity> = withTransaction { AbsenceEntity.all() }
+        assertTrue { withTransaction { absences.count().toInt() } == 4 }
+
+        var services: SizedIterable<ServiceEntity> = withTransaction { ServiceEntity.all() }
+        assertTrue { withTransaction { services.count().toInt() } == 2 }
+
+        val deleteResponse = client.delete(UsersAPI.UUID.Delete(UsersAPI.UUID(uuid = userUUID)))
+
+        assertEquals(
+            expected = HttpStatusCode.OK,
+            actual = deleteResponse.status
+        )
+
+        usersAPI = client.get(UsersAPI()).body()
+        usersAPI.forEach { println(it) }
+        assertTrue { usersAPI.isEmpty() }
+
+        absences = withTransaction { AbsenceEntity.all() }
+        assertTrue { withTransaction { absences.count().toInt() } == 2 }
+
+        services = withTransaction { ServiceEntity.all() }
+        assertTrue { withTransaction { services.count().toInt() } == 2 }
+
+        withTransaction { services.forEach { assertTrue { it.assignedUser == null } } }
+
+        schedules = withTransaction { ScheduleEntity.all() }
+        assertTrue { withTransaction { schedules.count().toInt() } == 0 }
     }
 
     private fun setupTestDBAndFlyway(): DbConnection {
