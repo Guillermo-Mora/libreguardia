d431950e2200853e625eaeaa6e24bc7ae28474ad
Carpinjod <carpinjod@alu.edu.gva.es>
2026-04-07 11:50:37 +0200

    Add buildingRouting to configureRouting

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/config/Routing.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/config/Routing.kt
@@ -1,7 +1,9 @@
 package com.libreguardia.config
 
 
+import com.libreguardia.routing.buildingRouting
 import com.libreguardia.routing.userRouting
+import com.libreguardia.service.BuildingService
 import com.libreguardia.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -9,6 +11,7 @@ import io.ktor.server.resources.Resources
 import io.ktor.server.routing.*
 
 fun Application.configureRouting(
+    buildingService: BuildingService,
     userService: UserService
 ) {
     //Unified routes for pages and for obtaining content (get, post, put, patch)
@@ -16,6 +19,9 @@ fun Application.configureRouting(
     routing {
         userRouting(
             userService = userService
+        )
+        buildingRouting(
+            service = buildingService
         )
 
 
