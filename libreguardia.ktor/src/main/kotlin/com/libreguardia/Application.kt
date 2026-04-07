07b0412d68b074190efeff63d3ee4920a4d20441
Carpinjod <carpinjod@alu.edu.gva.es>
2026-04-07 11:52:28 +0200

    Wire BuildingService in Application

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/Application.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/Application.kt
@@ -2,16 +2,64 @@ package com.libreguardia
 
 import com.libreguardia.config.*
 import com.libreguardia.repository.AbsenceRepository
+import com.libreguardia.repository.BuildingRepository
 import com.libreguardia.repository.ScheduleRepository
 import com.libreguardia.repository.ServiceRepository
 import com.libreguardia.repository.UserRepository
 import com.libreguardia.repository.UserRoleRepository
+import com.libreguardia.service.BuildingService
 import com.libreguardia.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
+}
+
+fun Application.module() {
+    val config = environment.config
+    val dbUrl = config.property("storage.jdbcURL").getString()
+    val dbUser = config.property("storage.user").getString()
+    val dbPassword = config.property("storage.password").getString()
+
+    val userRepository = UserRepository()
+    val absenceRepository = AbsenceRepository()
+    val serviceRepository = ServiceRepository()
+    val scheduleRepository = ScheduleRepository()
+    val userRoleRepository = UserRoleRepository()
+    val buildingRepository = BuildingRepository()
+
+    val userService = UserService(
+        userRepository = userRepository,
+        absenceRepository = absenceRepository,
+        serviceRepository = serviceRepository,
+        scheduleRepository = scheduleRepository,
+        userRoleRepository = userRoleRepository
+    )
+    val buildingService = BuildingService(
+        repository = buildingRepository
+    )
+
+    configureDatabase(
+        url = dbUrl,
+        user = dbUser,
+        password = dbPassword
+    )
+    configureFlyway(
+        url = dbUrl,
+        user = dbUser,
+        password = dbPassword
+    )
+    configureMonitoring()
+    configureDefaultHeaders()
+    configureCompression()
+    configureStatusPages()
+    configureSerialization()
+    configureAuthentication()
+    configureRouting(
+        buildingService = buildingService,
+        userService = userService
+    )
 }
 
 //It would be better to separate the app in different modules in the future. For better responsability
