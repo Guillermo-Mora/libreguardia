595ee41cc81719c0040350416bca4b328ea59826
Carpinjod <carpinjod@alu.edu.gva.es>
2026-04-09 12:35:03 +0200

    Add Course routing configuration and exception handling

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/config/Routing.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/config/Routing.kt
@@ -1,7 +1,9 @@
 package com.libreguardia.config
 
 
+import com.libreguardia.routing.courseRouting
 import com.libreguardia.routing.userRouting
+import com.libreguardia.service.CourseService
 import com.libreguardia.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -9,13 +11,17 @@ import io.ktor.server.resources.Resources
 import io.ktor.server.routing.*
 
 fun Application.configureRouting(
-    userService: UserService
+    userService: UserService,
+    courseService: CourseService
 ) {
     //Unified routes for pages and for obtaining content (get, post, put, patch)
     install(Resources)
     routing {
         userRouting(
             userService = userService
+        )
+        courseRouting(
+            service = courseService
         )
 
 
