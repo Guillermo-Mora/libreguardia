46ce38b830f8d2ba6deffe80cbb19e7ca98c89a4
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-13 19:19:40 +0200

    Reworked some methods for better understanding and deleted unnecessary comments.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/service/UserService.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/service/UserService.kt
@@ -2,7 +2,6 @@ package com.libreguardia.service
 
 import at.favre.lib.crypto.bcrypt.BCrypt
 import com.libreguardia.config.BCRYPT_HASH_COST
-import com.libreguardia.db.model.UserRoleEntity
 import com.libreguardia.dto.UserCreateDTO
 import com.libreguardia.dto.UserEditDTO
 import com.libreguardia.dto.UserEditProfileDTO
@@ -10,9 +9,8 @@ import com.libreguardia.dto.UserResponseDTO
 import com.libreguardia.exception.IncorrectPasswordException
 import com.libreguardia.exception.UserAlreadyDeletedException
 import com.libreguardia.exception.UserNotFoundException
-import com.libreguardia.exception.UserRoleNotFoundException
 import com.libreguardia.repository.*
-import com.libreguardia.utils.withTransaction
+import com.libreguardia.util.withTransaction
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -27,24 +25,16 @@ class UserService (
     private val absenceRepository: AbsenceRepository,
     private val serviceRepository: ServiceRepository,
     private val scheduleRepository: ScheduleRepository,
-    private val userRoleRepository: UserRoleRepository,
     private val refreshTokenRepository: RefreshTokenRepository
 ) {
-    //[Admin]
-    suspend fun getAllUsers(): List<UserResponseDTO> = withTransaction {
-        userRepository.getAll()
-    }
+    suspend fun getAllUsers(): List<UserResponseDTO> =
+        withTransaction { userRepository.getAll() }
 
     suspend fun getUser(
         userUUID: UUID
-    ): UserResponseDTO {
-        return withTransaction {
-            val user = userRepository.getByUUID(userUUID) ?: throw UserNotFoundException()
-            user
-        }
-    }
+    ) =
+        withTransaction { userRepository.getByUUID(userUUID) ?: throw UserNotFoundException() }
 
-    //[Admin]
     suspend fun createUser(
         userCreateDTO: UserCreateDTO
     ) {
@@ -53,7 +43,6 @@ class UserService (
             userCreateDTO.password.toCharArray()
         )
         withTransaction {
-            if (!userRoleRepository.existsByUUID(userCreateDTO.userRoleUUID)) throw UserRoleNotFoundException()
             userRepository.save(
                 userCreateDTO = userCreateDTO,
                 hashedPassword = hashedPassword
@@ -61,41 +50,35 @@ class UserService (
         }
     }
 
-    //[Admin]
     suspend fun editUser(
         userUUID: UUID,
         userEditDTO: UserEditDTO
     ) {
         val dateTimeNow: LocalDateTime = clock.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        val hashedPassword = userEditDTO.password?.let { newPassword ->
+            bcryptHasher.hashToString(
+                BCRYPT_HASH_COST,
+                newPassword.toCharArray()
+            )
+        }
+        val shouldCloseSessions = userEditDTO.isEnabled == false || userEditDTO.role != null
         withTransaction {
-            if (userEditDTO.userRoleUUID != null) {
-                if (!userRoleRepository.existsByUUID(userEditDTO.userRoleUUID)) throw UserRoleNotFoundException()
-            }
-            val hashedPassword = userEditDTO.password?.let { newPassword ->
-                bcryptHasher.hashToString(
-                    BCRYPT_HASH_COST,
-                    newPassword.toCharArray()
-                )
-            }
             if (!userRepository.editByUUID(
                     userUUID = userUUID,
                     userEditDTO = userEditDTO,
                     hashedPassword = hashedPassword
                 )
             ) throw UserNotFoundException()
-            userEditDTO.isEnabled?.let {
-                if (!it) {
-                    serviceRepository.setNullAssignedServicesToUserUUIDAfterNow(
-                        userUUID = userUUID,
-                        dateTimeNow = dateTimeNow
-                    )
-                    refreshTokenRepository.deleteRefreshTokensByUser(userUUID)
-                }
+            if (userEditDTO.isEnabled == false) {
+                serviceRepository.setNullAssignedServicesToUserUUIDAfterNow(
+                    userUUID = userUUID,
+                    dateTimeNow = dateTimeNow
+                )
             }
+            if (shouldCloseSessions) refreshTokenRepository.deleteRefreshTokensByUser(userUUID = userUUID)
         }
     }
 
-    //[Admin]
     suspend fun deleteUser(
         userUUID: UUID
     ) {
@@ -129,7 +112,6 @@ class UserService (
         }
     }
 
-    //[Admin]
     suspend fun toggleEnableUser(
         userUUID: UUID,
         enableOrDisable: Boolean
