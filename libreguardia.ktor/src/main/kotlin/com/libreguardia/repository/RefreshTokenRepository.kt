dea4223290e80b4c87c1811cab0fabaef98c6193
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-13 19:16:37 +0200

    Added 2 more functions that I use in the Auth Service. And reworked the previous delete function to now return a Boolean type to the service layer, so I can see if the result is the expected or not.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/RefreshTokenRepository.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/RefreshTokenRepository.kt
@@ -2,6 +2,8 @@ package com.libreguardia.repository
 
 import com.libreguardia.db.model.RefreshTokenEntity
 import com.libreguardia.db.model.RefreshTokenTable
+import com.libreguardia.db.model.UserTable
+import org.jetbrains.exposed.v1.core.and
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.deleteWhere
 import org.jetbrains.exposed.v1.jdbc.insert
@@ -32,14 +34,35 @@ class RefreshTokenRepository {
 
     fun getRefreshTokenEntity(
         refreshTokenPrefix: String,
-    ): RefreshTokenEntity? {
-        return RefreshTokenEntity.find { RefreshTokenTable.refreshTokenPrefix eq refreshTokenPrefix }
+    ): RefreshTokenEntity? =
+        RefreshTokenEntity.find { RefreshTokenTable.refreshTokenPrefix eq refreshTokenPrefix }
             .limit(1).firstOrNull()
-    }
+
+    fun getRefreshTokenHash(
+        refreshTokenPrefix: String,
+        userUuid: UUID
+    ): String? = RefreshTokenTable
+        .select(RefreshTokenTable.refreshTokenHash)
+        .where {
+            RefreshTokenTable.refreshTokenPrefix eq refreshTokenPrefix and
+                    (RefreshTokenTable.id eq userUuid)
+        }
+        .limit(1)
+        .map { it[RefreshTokenTable.refreshTokenHash] }
+        .firstOrNull()
 
     fun deleteRefreshToken(
         uuid: UUID
-    ) {
-        RefreshTokenTable.deleteWhere { RefreshTokenTable.id eq uuid }
+    ): Boolean {
+        return RefreshTokenTable.deleteWhere { RefreshTokenTable.id eq uuid } == 1
     }
+
+    fun deleteRefreshToken(
+        refreshTokenPrefix: String,
+        userUUID: UUID
+    ): Boolean =
+        RefreshTokenTable.deleteWhere {
+            RefreshTokenTable.refreshTokenPrefix eq refreshTokenPrefix and
+                    (RefreshTokenTable.user eq userUUID)
+        } == 1
 }
