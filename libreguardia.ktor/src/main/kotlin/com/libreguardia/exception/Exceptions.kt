295f76e89f908a664055f312cc3626008f14a707
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-13 19:21:41 +0200

    - Added new Exception that will be thrown when trying to access and endpoint that requires a role that we don't have. Example: User with "USER" role tries to access the "createUser" endpoint, which is only available for users with the "ADMIN" role.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/Exceptions.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/Exceptions.kt
@@ -1,8 +1,9 @@
 package com.libreguardia.exception
 
-class UserRoleNotFoundException : RuntimeException()
+class RoleNotFoundException : RuntimeException()
 class UserNotFoundException : RuntimeException()
 class UserAlreadyDeletedException : RuntimeException()
 class IncorrectPasswordException : RuntimeException()
 class InvalidCredentialsException : RuntimeException()
 class InvalidRefreshTokenException : RuntimeException()
+class InsufficientPermissionsException : RuntimeException()
