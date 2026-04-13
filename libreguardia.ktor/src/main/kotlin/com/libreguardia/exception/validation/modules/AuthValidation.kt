0fd6dceb7d568c6301973305b457612233b5e67a
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-13 19:02:59 +0200

    - Implemented auth validation for the refresh token. To validate its length before working with it (32).

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/validation/modules/AuthValidation.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/validation/modules/AuthValidation.kt
@@ -1,6 +1,8 @@
 package com.libreguardia.exception.validation.modules
 
 import com.libreguardia.dto.LoginDTO
+import com.libreguardia.dto.RefreshTokenDTO
+import com.libreguardia.exception.validation.validateRefreshToken
 import com.libreguardia.exception.validation.validateResult
 import com.libreguardia.exception.validation.validateString
 import io.ktor.server.plugins.requestvalidation.*
@@ -11,5 +13,9 @@ fun RequestValidationConfig.authValidation() {
         validateString(it.email)?.let { error -> errors.add(error) }
         validateString(it.password)?.let { error -> errors.add(error) }
         return@validate validateResult(errors)
+    }
+    validate<RefreshTokenDTO> {
+        validateRefreshToken(it.refreshToken)?.let { error -> return@validate ValidationResult.Invalid(error) }
+        return@validate ValidationResult.Valid
     }
 }
