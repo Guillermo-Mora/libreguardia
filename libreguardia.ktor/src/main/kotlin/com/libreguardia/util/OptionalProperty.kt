62d56d42426043b1d62c9827b7cf60075d41fefd
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-13 19:14:48 +0200

    Added comment to remember in the future why I have this code.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/util/OptionalProperty.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/util/OptionalProperty.kt
@@ -1,10 +1,13 @@
-package com.libreguardia.utils
+package com.libreguardia.util
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+
+//Code copied from an Internet article. To create variables of type optionals. So I can differentiate between
+// null and missing in the DTOs sent. But currently I don't need this for anything.
 
 sealed class OptionalProperty<out T> {
 
