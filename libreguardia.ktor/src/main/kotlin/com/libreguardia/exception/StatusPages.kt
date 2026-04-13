52edfce19262bf49f3740976e81b81532d253661
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-13 19:24:40 +0200

    Added new error codes for errors of: - User tries to access role protected endpoint without the required role (INSUFFICIENT_PERMISSIONS). - User tries to create a new registry of any table using an already existing unique field value (DUPLICATED_UNIQUE_FIELD).
    - Implemented handling in the StatusPages for both of these errors.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/StatusPages.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/StatusPages.kt
@@ -1,12 +1,13 @@
 package com.libreguardia.exception
 
-import com.libreguardia.exception.ErrorCode
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
+import org.jetbrains.exposed.v1.exceptions.ExposedSQLException
+import org.postgresql.util.PSQLException
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
@@ -16,10 +17,10 @@ fun Application.configureStatusPages() {
                 message = cause.reasons.joinToString()
             )
         }
-        exception<UserRoleNotFoundException> { call, _ ->
+        exception<RoleNotFoundException> { call, _ ->
             call.respond(
                 status = HttpStatusCode.NotFound,
-                message = ErrorCode.USER_ROLE_NOT_FOUND
+                message = ErrorCode.ROLE_NOT_FOUND
             )
         }
         exception<UserNotFoundException> { call, _ ->
@@ -46,10 +47,34 @@ fun Application.configureStatusPages() {
                 message = ErrorCode.INVALID_CREDENTIALS
             )
         }
+        exception<ExposedSQLException> { call, cause ->
+            val sqlState = (cause.cause as PSQLException).sqlState
+            when (sqlState) {
+                "23505" -> call.respond(
+                    status = HttpStatusCode.Conflict,
+                    message = ErrorCode.DUPLICATED_UNIQUE_FIELD
+                )
+
+                else -> call.respond(
+                    //TEMPORARY FOR DEBUGGING
+                    status = HttpStatusCode.InternalServerError,
+                    //message = ErrorCode.UNKNOWN_SERVER_ERROR
+                    message = cause.cause.toString()
+                )
+            }
+        }
+        //En vez de responder así, se podría responder enviando una página htmx, cómo la del login.
+        // Y así redirigirlo ahí.
         exception<InvalidRefreshTokenException> { call, _ ->
             call.respond(
                 status = HttpStatusCode.Unauthorized,
                 message = ErrorCode.INVALID_REFRESH_TOKEN
+            )
+        }
+        exception<InsufficientPermissionsException> { call, _ ->
+            call.respond(
+                status = HttpStatusCode.Forbidden,
+                message = ErrorCode.INSUFFICIENT_PERMISSIONS
             )
         }
         //TEMPORARY FOR DEBUGGING
