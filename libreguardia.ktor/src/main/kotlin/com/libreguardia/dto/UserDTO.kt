0535336a159e54eac1a78792fae8c2eeb1291698
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-16 20:11:38 +0200

    Removed unnecesary DTO and mapping, as the content sent to the frontend will be directly HTML.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/UserDTO.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/UserDTO.kt
@@ -1,23 +1,8 @@
 package com.libreguardia.dto
 
-import com.libreguardia.util.UUIDSerializer
 import com.libreguardia.db.model.UserEntity
 import com.libreguardia.repository.UserRepository
 import kotlinx.serialization.Serializable
-import java.util.UUID
-
-@Serializable
-data class UserResponseDTO(
-    @Serializable(with = UUIDSerializer::class)
-    val id: UUID,
-    val name: String,
-    val surname: String,
-    val email: String,
-    val phoneNumber: String,
-    val isEnabled: Boolean,
-    val isDeleted: Boolean,
-    val role: String
-)
 
 @Serializable
 data class UserCreateDTO(
@@ -46,17 +31,4 @@ data class UserEditProfileDTO(
     val phoneNumber: String? = null,
     val currentPassword: String? = null,
     val newPassword: String? = null,
-)
-
-fun UserRepository.entityToResponse(
-    entity: UserEntity
-) = UserResponseDTO(
-    id = entity.id.value,
-    name = entity.name,
-    surname = entity.surname,
-    email = entity.email,
-    phoneNumber = entity.phoneNumber,
-    isEnabled = entity.isEnabled,
-    isDeleted = entity.isDeleted,
-    role = entity.role.toString()
 )
