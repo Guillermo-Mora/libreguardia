427f9cc783f7cdce60c3027abb14409b8c8e6c97
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-16 20:10:30 +0200

    Implemented the new auth validations for logging in and for accessing protected pages. - Auth form for validating the user email and password sent to the form, and sending a valid cookie. - Auth session for validating the cookie sent for accessing protected content.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/config/Security.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/config/Security.kt
@@ -1,21 +1,74 @@
 package com.libreguardia.config
 
-import com.libreguardia.service.JwtService
-import io.ktor.server.application.Application
-import io.ktor.server.auth.authentication
-import io.ktor.server.auth.jwt.jwt
+import com.libreguardia.exception.ErrorCode
+import com.libreguardia.service.AuthService
+import com.libreguardia.util.UUIDSerializer
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondRedirect
+import io.ktor.server.sessions.*
+import kotlinx.serialization.Serializable
+import java.util.*
 
+const val COOKIE_DURATION: Long = 2_592_000
 const val BCRYPT_HASH_COST = 10
 fun Application.configureSecurity(
-    jwtService: JwtService
+    authService: AuthService
 ) {
-    authentication {
-        jwt {
-            realm = jwtService.realm
-            verifier(jwtService.jwtVerifier)
-            validate { credential ->
-                jwtService.customValidator(credential)
+    install(Sessions) {
+        cookie<UserSession>(
+            name = "user_session"
+        ) {
+            cookie.path = "/"
+            cookie.maxAgeInSeconds = COOKIE_DURATION
+            cookie.httpOnly = true
+            //secure = true,
+            //Temporary for development
+            cookie.secure = false
+        }
+    }
+
+    install(Authentication) {
+        form(
+            name = "auth-form"
+        ) {
+            userParamName = "email"
+            passwordParamName = "password"
+            validate { credentials ->
+                if (authService.validateLogin(credentials))
+                    UserIdPrincipal(name = credentials.name)
+                else null
+            }
+            challenge {
+                call.respond(
+                    status = HttpStatusCode.Unauthorized,
+                    message = ErrorCode.INVALID_CREDENTIALS
+                )
+            }
+        }
+
+        session<UserSession>(
+            name = "auth-session"
+        ) {
+            validate { session ->
+                if (authService.validateSession(session))
+                    session
+                else null
+            }
+            challenge {
+                call.respondRedirect("/login")
             }
         }
     }
 }
+
+@Serializable
+data class UserSession (
+    @Serializable(with = UUIDSerializer::class)
+    val uuid: UUID,
+    @Serializable(with = UUIDSerializer::class)
+    val userUuid: UUID
+)
+
