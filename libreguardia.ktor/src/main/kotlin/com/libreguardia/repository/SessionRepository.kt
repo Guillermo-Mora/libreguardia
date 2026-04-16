c6b905006b572074c243c18b1cf06a0accf7c3dc
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-16 20:06:30 +0200

    Aded the session table, entity, and repository. The session table stores each user session. I use it to manage if a session is valid by its expiration date, and associated user. With this, I can later implement log out, session blocking, logout on all devices / specific devices, logout all devices on password change, etc.

---
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/SessionRepository.kt
@@ -0,0 +1,29 @@
+package com.libreguardia.repository
+
+import com.libreguardia.db.model.SessionEntity
+import com.libreguardia.db.model.SessionTable
+import com.libreguardia.db.model.UserEntity
+import org.jetbrains.exposed.v1.dao.load
+import org.jetbrains.exposed.v1.jdbc.insert
+import java.util.UUID
+import kotlin.time.Instant
+
+class SessionRepository {
+    fun save(
+        uuid: UUID,
+        expiration: Instant,
+        userUuid: UUID
+    ) {
+        SessionTable.insert {
+            it[id] = uuid
+            it[expiresAt] = expiration
+            it[user] = userUuid
+        }
+    }
+
+    fun getSessionEntityWithUserLoaded(
+        uuid: UUID
+    ): SessionEntity? {
+        return SessionEntity.findById(uuid)?.load(SessionEntity::user)
+    }
+}
