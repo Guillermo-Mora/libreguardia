f7011434b527478a5c90fcc89f403c89e3bd5721
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-17 11:17:36 +0200

    Eliminated user already deleted error code. As adding an error handling for trying to delete a user that already is soft-deleted is unnecessary and adds complexity.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/Exceptions.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/exception/Exceptions.kt
@@ -2,7 +2,6 @@ package com.libreguardia.exception
 
 class RoleNotFoundException : RuntimeException()
 class UserNotFoundException : RuntimeException()
-class UserAlreadyDeletedException : RuntimeException()
 class IncorrectPasswordException : RuntimeException()
 class InvalidCredentialsException : RuntimeException()
 class InvalidRefreshTokenException : RuntimeException()
