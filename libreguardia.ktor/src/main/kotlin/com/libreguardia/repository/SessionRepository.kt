af9af552a5749532b1fd4fd7e11c4cb4edd254dd
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-17 11:25:48 +0200

    - Implemented some useful functions for deleteing all sessions and getting a session with its user loaded. - The one that gets the session with its user loaded is used to validate the session on session protected endpoints. - While the deleteSessionFromUser functions servers the same purpose as the deleteAllRefreshTokensFromUser function, which was in the refreshTokenRepository. This way, if an user role is changed or user is deleted / soft deleted / disabled, all his active sessions will be deleted from the DB. meaning that he will have to login again in the application.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/SessionRepository.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/SessionRepository.kt
@@ -3,7 +3,9 @@ package com.libreguardia.repository
 import com.libreguardia.db.model.SessionEntity
 import com.libreguardia.db.model.SessionTable
 import com.libreguardia.db.model.UserEntity
+import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.dao.load
+import org.jetbrains.exposed.v1.jdbc.deleteWhere
 import org.jetbrains.exposed.v1.jdbc.insert
 import java.util.UUID
 import kotlin.time.Instant
@@ -24,6 +26,15 @@ class SessionRepository {
     fun getSessionEntityWithUserLoaded(
         uuid: UUID
     ): SessionEntity? {
-        return SessionEntity.findById(uuid)?.load(SessionEntity::user)
+        return SessionEntity
+            .findById(uuid)
+            ?.load(SessionEntity::user)
+    }
+
+    fun deleteSessionsFromUser(
+        userUuid: UUID
+    ) {
+        SessionTable
+            .deleteWhere { SessionTable.user eq userUuid }
     }
 }
