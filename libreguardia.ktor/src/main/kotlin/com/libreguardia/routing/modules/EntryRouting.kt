bd57ba4d3ac8bc7a538656d0e3ff7ad2ffe8aad2
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-17 22:08:22 +0200

    Implemented main page view, available for all 3 types of users. Visualizer will see a display of all the current Services states with lives updates(Still not implemented)

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/modules/EntryRouting.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/modules/EntryRouting.kt
@@ -1,6 +1,9 @@
 package com.libreguardia.routing.modules
 
+import com.libreguardia.config.AUTH_SESSION
 import com.libreguardia.config.UserPrincipal
+import com.libreguardia.config.authorized
+import com.libreguardia.db.Role
 import com.libreguardia.exception.InsufficientPermissionsException
 import com.libreguardia.frontend.page.mainPage
 import com.libreguardia.frontend.page.loginPage
@@ -14,11 +17,13 @@ fun Route.entryRouting() {
     get("/login") {
         call.respondHtml { loginPage() }
     }
-    authenticate ("auth-session") {
-        get("/") {
-            val role = call.principal<UserPrincipal>()
-                ?.userRole ?: throw InsufficientPermissionsException()
-            call.respondHtml { mainPage(role = role) }
+    authenticate(AUTH_SESSION) {
+        authorized(Role.USER, Role.ADMIN, Role.VISUALIZER) {
+            get("/") {
+                val role = call.principal<UserPrincipal>()
+                    ?.userRole ?: throw InsufficientPermissionsException()
+                call.respondHtml { mainPage(role = role) }
+            }
         }
     }
 }
