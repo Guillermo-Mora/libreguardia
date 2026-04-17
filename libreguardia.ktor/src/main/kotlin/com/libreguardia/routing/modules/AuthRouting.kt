d0cab3b42ac38b8b505e8905f4c13156b705fb34
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-17 21:48:58 +0200

    Implemented logout and logout all devices functions. Currently working perfectly

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/modules/AuthRouting.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/modules/AuthRouting.kt
@@ -1,40 +1,38 @@
 package com.libreguardia.routing.modules
 
+import com.libreguardia.config.AUTH_FORM
+import com.libreguardia.config.AUTH_SESSION
+import com.libreguardia.config.UserPrincipal
 import com.libreguardia.config.UserSession
+import com.libreguardia.config.authorized
 import com.libreguardia.db.Role
-import com.libreguardia.dto.RefreshTokenDTO
 import com.libreguardia.exception.InvalidCredentialsException
+import com.libreguardia.exception.UserNotFoundException
 import com.libreguardia.service.AuthService
-import com.libreguardia.util.userPrincipal
-import com.libreguardia.util.userUuidFromJwt
-import io.ktor.http.HttpStatusCode
 import io.ktor.resources.*
-import io.ktor.server.auth.UserIdPrincipal
-import io.ktor.server.auth.authenticate
-import io.ktor.server.auth.principal
-import io.ktor.server.request.*
+import io.ktor.server.auth.*
+import io.ktor.server.request.receive
 import io.ktor.server.resources.post
 import io.ktor.server.response.*
 import io.ktor.server.routing.Route
-import io.ktor.server.sessions.sessions
-import io.ktor.server.sessions.set
+import io.ktor.server.sessions.*
 
-@Resource("/api/auth")
+@Resource("/auth")
 class AuthAPI {
     @Resource("login")
-    class Login (val parent: AuthAPI = AuthAPI())
+    class Login(val parent: AuthAPI = AuthAPI())
+
     @Resource("logout")
-    class Logout (val parent: AuthAPI = AuthAPI())
+    class Logout(val parent: AuthAPI = AuthAPI())
+
     @Resource("logout-all-devices")
-    class LogoutAllDevices (val parent: AuthAPI = AuthAPI())
-    @Resource("refresh-token")
-    class RefreshToken (val parent: AuthAPI = AuthAPI())
+    class LogoutAllDevices(val parent: AuthAPI = AuthAPI())
 }
 
 fun Route.authRouting(
     authService: AuthService
 ) {
-    authenticate("auth-form") {
+    authenticate(AUTH_FORM) {
         post<AuthAPI.Login> {
             val userEmail: String =
                 call.principal<UserIdPrincipal>()?.name ?: throw InvalidCredentialsException()
@@ -43,27 +41,25 @@ fun Route.authRouting(
             call.respondRedirect("/")
         }
     }
-}
-/*
-    authenticate {
+    authenticate(AUTH_SESSION) {
         authorized(Role.USER, Role.ADMIN) {
             post<AuthAPI.Logout> {
-                val refreshToken = call.receive<RefreshTokenDTO>()
-                val userPrincipal = call.userPrincipal()
+                //TEMPORARY ERROR THROWN, THIS SHOULD BE HANDLED IN A BETTER WAY
+                val sessionUuid = call.sessions.get<UserSession>()?.uuid ?: throw UserNotFoundException()
                 authService.logout(
-                    refreshTokenDTO = refreshToken,
-                    userUuid = userPrincipal.uuid
+                    sessionUuid = sessionUuid
                 )
-                call.respond(HttpStatusCode.NoContent)
+                call.response.headers.append("HX-Redirect", "/login")
             }
             post<AuthAPI.LogoutAllDevices> {
-                val userUuid = call.userUuidFromJwt()
+                //TEMPORARY ERROR THROWN, THIS SHOULD BE HANDLED IN A BETTER WAY
+                println(call.principal<UserPrincipal>())
+                val userUuid = call.principal<UserPrincipal>()?.userUuid ?: throw UserNotFoundException()
                 authService.logoutAllDevices(
                     userUuid = userUuid
                 )
-                call.respond(HttpStatusCode.NoContent)
+                call.response.headers.append("HX-Redirect", "/login")
             }
         }
     }
-
- */
+}
--- libreguardia.ktor/src/main/kotlin/com/libreguardia/service/AuthService.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/service/AuthService.kt
@@ -4,7 +4,6 @@ import at.favre.lib.crypto.bcrypt.BCrypt
 import com.libreguardia.config.COOKIE_DURATION
 import com.libreguardia.config.UserPrincipal
 import com.libreguardia.config.UserSession
-import com.libreguardia.db.Role
 import com.libreguardia.exception.InvalidCredentialsException
 import com.libreguardia.repository.SessionRepository
 import com.libreguardia.repository.UserRepository
@@ -12,16 +11,13 @@ import com.libreguardia.util.withTransaction
 import io.ktor.server.auth.UserPasswordCredential
 import java.util.*
 import kotlin.time.Clock
-import kotlin.time.Duration.Companion.days
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-private val COOKIE_DURATION = 30.days
 class AuthService(
     private val bcryptVerifyer: BCrypt.Verifyer,
     private val bcryptHasher: BCrypt.Hasher,
     private val clock: Clock.System,
-    private val jwtService: JwtService,
     private val userRepository: UserRepository,
     private val sessionRepository: SessionRepository
 ) {
@@ -40,8 +36,6 @@ class AuthService(
     suspend fun validateSession(
         userSession: UserSession
     ): UserPrincipal? {
-        //Esta función debería de devolver de alguna forma el rol y el uuid del usuario,
-        // para que el plugin custom de roles, y las páginas puedan usarlo para mostrar el contenido adecuado
         return withTransaction {
             val sessionEntity = sessionRepository.getSessionEntityWithUserLoaded(uuid = userSession.uuid)
                 ?: return@withTransaction null
@@ -73,110 +67,12 @@ class AuthService(
             )
         }
     }
-}
-
-    //My first authentication implementation, using JWT (not usable with HTMX, which should be managed with cookies)
-    // I didn't investigate how HTMX authentication was commonly implemented, so I implemented my own JWT
-    // auth thinking I could easily use it with HTMX frontend
-    /*
-    suspend fun validateLogin(
-        credentials: LoginDTO
-    ): LoginResponseDTO {
-        val fakeHashedPassword = $$"$2a$10$ppBBdNODqqvrTLd4wVAQ0OJ7i0hsmuJYcy69g/pPlfzmBD/pEPTMK"
-        val userEntity = withTransaction { userRepository.getEntity(credentials.email) }
-        val verificationResult = bcryptVerifyer.verify(
-            credentials.password.toByteArray(),
-            (userEntity?.password ?: fakeHashedPassword).toByteArray()
-        )
-        if (!verificationResult.verified || userEntity?.isEnabled != true || userEntity.isDeleted)
-            throw InvalidCredentialsException()
-        val accessToken = jwtService.createAccessToken(
-            uuid = userEntity.id.value,
-            role = userEntity.role
-        )
-        val refreshToken = createRefreshToken()
-        val refreshTokenPrefix = refreshToken.substring(0, REFRESH_TOKEN_PREFIX_LENGTH)
-        val refreshTokenHash = bcryptHasher.hashToString(
-            BCRYPT_HASH_COST,
-            refreshToken.substring(REFRESH_TOKEN_PREFIX_LENGTH).toCharArray()
-        )
-        withTransaction {
-            refreshTokenRepository.save(
-                newRefreshTokenPrefix = refreshTokenPrefix,
-                newRefreshTokenHash = refreshTokenHash,
-                expiration = clock.now().plus(REFRESH_TOKEN_DURATION),
-                userUuid = userEntity.id.value
-            )
-        }
-        return LoginResponseDTO(
-            accessToken = accessToken,
-            refreshToken = refreshToken
-        )
-    }
-
-    suspend fun refreshToken(
-        refreshTokenDTO: RefreshTokenDTO
-    ): LoginResponseDTO {
-        val refreshTokenPrefix = refreshTokenDTO.refreshToken.substring(0, REFRESH_TOKEN_PREFIX_LENGTH)
-        val refreshTokenEntity = withTransaction { refreshTokenRepository.getRefreshTokenEntity(refreshTokenPrefix) }
-            ?: throw InvalidRefreshTokenException()
-        val refreshTokenHash = refreshTokenDTO.refreshToken.substring(REFRESH_TOKEN_PREFIX_LENGTH)
-        if (refreshTokenEntity.expiresAt <= clock.now()) {
-            withTransaction { refreshTokenRepository.deleteRefreshToken(refreshTokenEntity.id.value) }
-            throw InvalidRefreshTokenException()
-        }
-        validateRefreshToken(
-            refreshToken = refreshTokenHash,
-            hashedRefreshToken = refreshTokenEntity.refreshTokenHash
-        )
-        val newRefreshToken = createRefreshToken()
-        val newRefreshTokenPrefix = newRefreshToken.substring(0, REFRESH_TOKEN_PREFIX_LENGTH)
-        val newRefreshTokenHash = bcryptHasher.hashToString(
-            BCRYPT_HASH_COST,
-            newRefreshToken.substring(REFRESH_TOKEN_PREFIX_LENGTH).toCharArray()
-        )
-        withTransaction {
-            refreshTokenRepository.save(
-                newRefreshTokenPrefix = newRefreshTokenPrefix,
-                newRefreshTokenHash = newRefreshTokenHash,
-                expiration = clock.now().plus(REFRESH_TOKEN_DURATION),
-                userUuid = refreshTokenEntity.user.id.value
-            )
-            if (!refreshTokenRepository.deleteRefreshToken(refreshTokenEntity.id.value))
-                throw InvalidRefreshTokenException()
-        }
-        val accessToken = jwtService.createAccessToken(
-            uuid = refreshTokenEntity.user.id.value,
-            role = refreshTokenEntity.user.role
-        )
-        return LoginResponseDTO(
-            accessToken = accessToken,
-            refreshToken = newRefreshToken
-        )
-    }
 
     suspend fun logout(
-        refreshTokenDTO: RefreshTokenDTO,
-        userUuid: UUID
+        sessionUuid: UUID
     ) {
-        val refreshTokenPrefix = refreshTokenDTO.refreshToken.substring(0, REFRESH_TOKEN_PREFIX_LENGTH)
-        val refreshTokenHash = refreshTokenDTO.refreshToken.substring(REFRESH_TOKEN_PREFIX_LENGTH)
-        val hashedRefreshToken = withTransaction {
-            refreshTokenRepository.getRefreshTokenHash(
-                refreshTokenPrefix = refreshTokenPrefix,
-                userUuid = userUuid
-            )
-        } ?: throw InvalidRefreshTokenException()
-        validateRefreshToken(
-            refreshToken = refreshTokenHash,
-            hashedRefreshToken = hashedRefreshToken
-        )
         withTransaction {
-            if (!refreshTokenRepository.deleteRefreshToken(
-                    refreshTokenPrefix = refreshTokenPrefix,
-                    userUUID = userUuid
-                )
-            ) throw InvalidRefreshTokenException()
+            sessionRepository.deleteSession(uuid = sessionUuid)
         }
     }
 
@@ -184,19 +80,7 @@ class AuthService(
         userUuid: UUID
     ) {
         withTransaction {
-            refreshTokenRepository.deleteRefreshTokensByUser(userUUID = userUuid)
+            sessionRepository.deleteSessionsFromUser(userUuid = userUuid)
         }
     }
-
-    private fun createRefreshToken(): String = UUID.randomUUID().toString().replace("-", "")
-    private fun validateRefreshToken(
-        refreshToken: String,
-        hashedRefreshToken: String
-    ) {
-        val verificationResult = bcryptVerifyer.verify(
-            refreshToken.toByteArray(),
-            hashedRefreshToken.toByteArray()
-        )
-        if (!verificationResult.verified) throw InvalidRefreshTokenException()
-    }
-     */
+}
