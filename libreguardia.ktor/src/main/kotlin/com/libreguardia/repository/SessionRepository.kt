1c9c12094ab63be76eaefbc285171e562ef6b767
Guillermo-Mora <guimormor@alu.edu.gva.es>
2026-04-20 10:23:18 +0200

    Optimized validation requests when accessing protected resources. Since this action is performed on every request to protected resources, is very important that the operation is as fast as possible. Previously, this operation was done using entities, getting 2 entities: 1 for the session and 1 for the user. This resulted in 2 separated queries that retrieved all the columns from both tables.
    In order to make this faster and more efficient, I added a new function in the SessionRepository, which retrieves only the necessary data using Exposed DSL, and then maps it to a SessionModel object. This results in a single SQL query, selecting only the necessary columns.

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/SessionRepository.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/repository/SessionRepository.kt
@@ -1,12 +1,19 @@
 package com.libreguardia.repository
 
+import com.libreguardia.db.model.GroupTable
+import com.libreguardia.db.model.ScheduleTable
 import com.libreguardia.db.model.SessionEntity
 import com.libreguardia.db.model.SessionTable
 import com.libreguardia.db.model.UserEntity
+import com.libreguardia.db.model.UserTable
+import com.libreguardia.model.SessionModel
+import com.libreguardia.model.dataToModel
+import org.jetbrains.exposed.v1.core.JoinType
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.dao.load
 import org.jetbrains.exposed.v1.jdbc.deleteWhere
 import org.jetbrains.exposed.v1.jdbc.insert
+import org.jetbrains.exposed.v1.jdbc.select
 import java.util.UUID
 import kotlin.time.Instant
 
@@ -29,6 +36,37 @@ class SessionRepository {
         return SessionEntity
             .findById(uuid)
             ?.load(SessionEntity::user)
+    }
+
+    fun getUserSessionModel(
+        uuid: UUID
+    ): SessionModel? {
+        return SessionTable
+            .join(
+                otherTable = UserTable,
+                joinType = JoinType.INNER,
+                onColumn = SessionTable.user,
+                otherColumn = UserTable.id
+            )
+            .select(
+                SessionTable.expiresAt,
+                UserTable.isEnabled,
+                UserTable.isDeleted,
+                UserTable.id,
+                UserTable.role
+            )
+            .where { SessionTable.id eq uuid }
+            .limit(1)
+            .map {
+                SessionModel(
+                    expiresAt = it[SessionTable.expiresAt],
+                    isEnabled = it[UserTable.isEnabled],
+                    isDeleted = it[UserTable.isDeleted],
+                    userUuid = it[UserTable.id].value,
+                    userRole = it[UserTable.role]
+                )
+            }
+            .firstOrNull()
     }
 
     fun deleteSessionsFromUser(
--- libreguardia.ktor/src/main/kotlin/com/libreguardia/service/AuthService.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/service/AuthService.kt
@@ -9,6 +9,7 @@ import com.libreguardia.repository.SessionRepository
 import com.libreguardia.repository.UserRepository
 import com.libreguardia.util.withTransaction
 import io.ktor.server.auth.UserPasswordCredential
+import org.jetbrains.exposed.v1.core.StdOutSqlLogger
 import java.util.*
 import kotlin.time.Clock
 import kotlin.time.DurationUnit
@@ -36,6 +37,23 @@ class AuthService(
     suspend fun validateSession(
         userSession: UserSession
     ): UserPrincipal? {
+        val userSessionModel = withTransaction {
+            //Replaced the previous operation using entities to now DSL. As this
+            // validation is performed on every request to protected resources,
+            // so it is key that is as efficient as possible.
+            addLogger(StdOutSqlLogger)
+            sessionRepository.getUserSessionModel(uuid = userSession.uuid)
+        } ?: return null
+        if (
+            userSessionModel.expiresAt <= clock.now() ||
+            !userSessionModel.isEnabled ||
+            userSessionModel.isDeleted
+        ) return null
+        return UserPrincipal(
+            userUuid = userSessionModel.userUuid,
+            userRole = userSessionModel.userRole
+        )
+        /*
         return withTransaction {
             val sessionEntity = sessionRepository.getSessionEntityWithUserLoaded(uuid = userSession.uuid)
                 ?: return@withTransaction null
@@ -49,6 +67,8 @@ class AuthService(
                 userRole = sessionEntity.user.role
             )
         }
+
+         */
     }
 
     suspend fun saveSession(
