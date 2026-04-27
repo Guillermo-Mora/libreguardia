0dc19873f468304965712166d2aed6d80edc3f4f
Carpinjod <carpinjod@alu.edu.gva.es>
2026-04-27 12:28:49 +0200

    feat: add BuildingValidation and register in RequestValidation

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/validation/RequestValidation.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/validation/RequestValidation.kt
@@ -2,6 +2,7 @@ package com.libreguardia.validation
 
 import com.libreguardia.validation.modules.academicYearValidation
 import com.libreguardia.validation.modules.authValidation
+import com.libreguardia.validation.modules.buildingValidation
 import com.libreguardia.validation.modules.userValidation
 import io.ktor.server.application.*
 import io.ktor.server.plugins.requestvalidation.*
@@ -11,5 +12,7 @@ fun Application.configureRequestValidation() {
         authValidation()
         userValidation()
         academicYearValidation()
+        buildingValidation()
     }
+}
 }
