c5fa81c7b824049e7004f53be4e461515e6eff5c
Carpinjod <carpinjod@alu.edu.gva.es>
2026-04-27 12:35:02 +0200

    feat: register buildingRouting in configureRouting

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/Routing.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/Routing.kt
@@ -3,18 +3,22 @@ package com.libreguardia.routing
 
 import com.libreguardia.routing.modules.academicYearRouting
 import com.libreguardia.routing.modules.authRouting
+import com.libreguardia.routing.modules.buildingRouting
 import com.libreguardia.routing.modules.entryRouting
 import com.libreguardia.routing.modules.userRouting
 import com.libreguardia.service.AcademicYearService
 import com.libreguardia.service.AuthService
+import com.libreguardia.service.BuildingService
 import com.libreguardia.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.resources.*
 import io.ktor.server.routing.*
+
 fun Application.configureRouting(
     authService: AuthService,
     academicYearService: AcademicYearService,
+    buildingService: BuildingService,
     userService: UserService
 ) {
     //Unified routes for pages and for obtaining content (get, post, put, patch)
@@ -28,6 +32,7 @@ fun Application.configureRouting(
             userService = userService
         )
         academicYearRouting(service = academicYearService)
+        buildingRouting(service = buildingService)
         staticResources("/static", "static")
     }
 }
