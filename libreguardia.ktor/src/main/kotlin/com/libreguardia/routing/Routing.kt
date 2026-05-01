b16f2cc815636f089327a828d12df55c12092787
adrian <anayaserraadrian@gmail.com>
2026-05-01 18:02:09 +0200

    implement routing endpoints

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/Routing.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/routing/Routing.kt
@@ -4,10 +4,12 @@ package com.libreguardia.routing
 import com.libreguardia.routing.modules.academicYearRouting
 import com.libreguardia.routing.modules.authRouting
 import com.libreguardia.routing.modules.entryRouting
+import com.libreguardia.routing.modules.scheduleActivityRouting
 import com.libreguardia.routing.modules.userRouting
 import com.libreguardia.routing.modules.validationRouting
 import com.libreguardia.service.AcademicYearService
 import com.libreguardia.service.AuthService
+import com.libreguardia.service.ScheduleActivityService
 import com.libreguardia.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
@@ -16,6 +18,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting(
     authService: AuthService,
     academicYearService: AcademicYearService,
+    scheduleActivityService: ScheduleActivityService,
     userService: UserService
 ) {
     //Unified routes for pages and for obtaining content (get, post, put, patch)
@@ -30,6 +33,7 @@ fun Application.configureRouting(
             userService = userService
         )
         academicYearRouting(service = academicYearService)
+        scheduleActivityRouting(service = scheduleActivityService)
         staticResources("/static", "static")
     }
 }
