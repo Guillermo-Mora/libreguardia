730848d2bc0798134b42af6b017b375983c9b7cd
adrian <anayaserraadrian@gmail.com>
2026-05-01 13:36:59 +0200

    add PlaceTypeRepository and PlaceTypeService

---
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/service/PlaceTypeService.kt
@@ -0,0 +1,34 @@
+package com.libreguardia.service
+
+import com.libreguardia.dto.PlaceTypeCreateDTO
+import com.libreguardia.dto.PlaceTypeEditDTO
+import com.libreguardia.dto.PlaceTypeResponseDTO
+import com.libreguardia.exception.PlaceTypeNotFoundException
+import com.libreguardia.repository.PlaceTypeRepository
+import com.libreguardia.util.withTransaction
+import java.util.UUID
+
+class PlaceTypeService(
+    private val placeTypeRepository: PlaceTypeRepository
+) {
+    suspend fun getAll(): List<PlaceTypeResponseDTO> = withTransaction { placeTypeRepository.getAll() }
+
+    suspend fun getByUUID(uuid: UUID): PlaceTypeResponseDTO =
+        withTransaction { placeTypeRepository.getByUUID(uuid) } ?: throw PlaceTypeNotFoundException()
+
+    suspend fun create(dto: PlaceTypeCreateDTO) {
+        withTransaction { placeTypeRepository.save(dto) }
+    }
+
+    suspend fun update(uuid: UUID, dto: PlaceTypeEditDTO) {
+        withTransaction {
+            if (!placeTypeRepository.update(uuid, dto)) throw PlaceTypeNotFoundException()
+        }
+    }
+
+    suspend fun delete(uuid: UUID) {
+        withTransaction {
+            if (!placeTypeRepository.delete(uuid)) throw PlaceTypeNotFoundException()
+        }
+    }
+}
