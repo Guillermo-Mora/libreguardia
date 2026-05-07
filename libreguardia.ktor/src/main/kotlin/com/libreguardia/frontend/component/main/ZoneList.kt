6d1242f26694bdab004e82461e07e0294ffbbfc0
Carpinjod <carpinjod@alu.edu.gva.es>
2026-05-07 12:24:14 +0200

    Added ZoneList component

---
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/frontend/component/main/ZoneList.kt
@@ -0,0 +1,45 @@
+package com.libreguardia.frontend.component.main
+
+import com.libreguardia.dto.module.ZoneResponseDTO
+import io.ktor.htmx.html.hx
+import io.ktor.utils.io.ExperimentalKtorApi
+import kotlinx.html.*
+
+@OptIn(ExperimentalKtorApi::class)
+fun FlowContent.zoneList(
+    zones: List<ZoneResponseDTO>
+) {
+    button {
+        attributes.hx {
+            get = "/zone/new"
+            replaceUrl = "true"
+            pushUrl = "true"
+            target = "#main-content"
+            swap = "innerHTML"
+        }
+        text("New")
+    }
+    table("schedule-table") {
+        tr {
+            th {
+                text("Name")
+            }
+        }
+        for (zone in zones)
+            tr {
+                td("td-filled") { text(zone.name) }
+                td {
+                    button {
+                        attributes.hx {
+                            get = "/zone/${zone.id}"
+                            replaceUrl = "true"
+                            pushUrl = "true"
+                            target = "#main-content"
+                            swap = "innerHTML"
+                        }
+                        text("Edit")
+                    }
+                }
+            }
+    }
+}
