dd3ceb6561cb300900cc67c817be5031a32087d3
Carpinjod <carpinjod@alu.edu.gva.es>
2026-05-07 12:25:01 +0200

    Added parameter conversion functions for Zone forms

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/module/ZoneDTO.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/module/ZoneDTO.kt
@@ -1,7 +1,11 @@
 package com.libreguardia.dto.module
 
 import com.libreguardia.db.model.ZoneEntity
+import com.libreguardia.dto.string
+import com.libreguardia.frontend.component.main.ZoneCreateField
+import com.libreguardia.frontend.component.main.ZoneEditField
 import com.libreguardia.util.UUIDSerializer
+import io.ktor.http.Parameters
 import kotlinx.serialization.Serializable
 import java.util.UUID
 
@@ -26,3 +30,18 @@ fun ZoneEntity.toResponseDTO() = ZoneResponseDTO(
     id = id.value,
     name = name
 )
+
+fun Parameters.toZoneCreateDTO() =
+    ZoneCreateDTO(
+        name = string(ZoneCreateField.NAME)
+    )
+
+fun Parameters.toZoneEditDTO() =
+    ZoneEditDTO(
+        name = string(ZoneEditField.NAME).takeIf { it.isNotBlank() }
+    )
+
+fun ZoneResponseDTO.toZoneEditDTO() =
+    ZoneEditDTO(
+        name = this.name
+    )
