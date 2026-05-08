ff4153efda359d9b2467952e32b3d3079a64b48a
Carpinjod <carpinjod@alu.edu.gva.es>
2026-05-08 10:18:48 +0200

    Added ScheduleActivityEdit form component

---
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/frontend/component/main/ScheduleActivityEdit.kt
@@ -0,0 +1,41 @@
+package com.libreguardia.frontend.component.main
+
+import com.libreguardia.dto.module.ScheduleActivityEditDTO
+import com.libreguardia.frontend.component.*
+import kotlinx.html.FlowContent
+import kotlinx.html.InputType
+import java.util.*
+
+enum class ScheduleActivityEditField(override val key: String) : FormField {
+    NAME("name"),
+    GENERATES_SERVICE("generates-service");
+}
+
+fun FlowContent.scheduleActivityEdit(
+    activity: ScheduleActivityEditDTO,
+    errors: Map<FormField, String?>? = null,
+    activityUuid: UUID
+) {
+    customForm(
+        formName = "edit-activity",
+        previousPagePath = "/schedule-activity",
+        operationType = OperationType.Patch,
+        operationPath = "/schedule-activity/${activityUuid}",
+        deletePath = "/schedule-activity/${activityUuid}",
+        errors = errors,
+        formFields = mapOf(
+            ScheduleActivityEditField.NAME to FormFieldData(
+                text = "name",
+                value = activity.name ?: "",
+                required = true,
+                inputType = InputType.text
+            ),
+            ScheduleActivityEditField.GENERATES_SERVICE to FormFieldData(
+                text = "generates service",
+                inputType = InputType.checkBox,
+                checkedValue = activity.generatesService ?: false,
+                required = true
+            )
+        ),
+    )
+}
