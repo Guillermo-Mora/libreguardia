5c8954d0d45a216bde677a7d2c88275405f6ed67
adrian <anayaserraadrian@gmail.com>
2026-05-10 20:26:38 +0200

    add Parameters mappers and toPlaceTypeEditDTO

--- libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/module/PlaceTypeDTO.kt
+++ libreguardia.ktor/src/main/kotlin/com/libreguardia/dto/module/PlaceTypeDTO.kt
@@ -1,7 +1,11 @@
 package com.libreguardia.dto.module
 
 import com.libreguardia.db.model.PlaceTypeEntity
+import com.libreguardia.dto.string
+import com.libreguardia.frontend.component.main.create.PlaceTypeCreateField
+import com.libreguardia.frontend.component.main.edit.PlaceTypeEditField
 import com.libreguardia.util.UUIDSerializer
+import io.ktor.http.Parameters
 import kotlinx.serialization.Serializable
 import java.util.UUID
 
@@ -26,3 +30,18 @@ fun PlaceTypeEntity.toResponseDTO() = PlaceTypeResponseDTO(
     id = id.value,
     name = name
 )
+
+fun Parameters.toPlaceTypeCreateDTO() =
+    PlaceTypeCreateDTO(
+        name = string(PlaceTypeCreateField.NAME)
+    )
+
+fun Parameters.toPlaceTypeEditDTO() =
+    PlaceTypeEditDTO(
+        name = string(PlaceTypeEditField.NAME).takeIf { it.isNotBlank() }
+    )
+
+fun PlaceTypeResponseDTO.toPlaceTypeEditDTO() =
+    PlaceTypeEditDTO(
+        name = this.name
+    )
